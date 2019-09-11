523593d3a5d7812fd249f0cb916c4c0c1af034be
dipakkailaspatil <patil.dipak363@gmail.com>
2019-09-12 00:40:02 +0530

    [Dipak] Add. check success by passing invalid credentials

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -2,12 +2,15 @@ const {check,validationResult}=require('express-validator');
 
 exports.logIn=(req,res)=> {
     
+    let responseResult={};
     check('email','Email Id Is not Valid').isEmail();
     check('password','Password is not Valid').isLength({min:8});
 
     let errors=validationResult;
 
     if(errors) {
-        res.status(422);
+        responseResult.success=false;
+        responseResult.message="Invalid Email Password";
+        res.status(422).send(responseResult);
     }
 }
--- server/test/LogInTest.js
+++ server/test/LogInTest.js
@@ -1,23 +1,39 @@
 
-const server=require('../Server');
-const chai=require('chai');
-const assert=require('assert');
-const chaihttp=require('chai-http');
+const server = require('../Server');
+const chai = require('chai');
+const assert = require('assert');
+const chaihttp = require('chai-http');
 
 chai.use(chaihttp);
 
-describe('Given EmailId "patildipak@gmailcom" and password "123" When Pass To LogIn API',()=>{
+describe('Given EmailId "patildipak@gmailcom" and password "123" When Pass To LogIn API', () => {
 
-    it('It will return Status Code 422',()=>{
-        let userData={
+    it('It will return Status Code 422', (done) => {
+        let userData = {
             email: "patildipak@gmailcom",
             password: "123"
         }
         chai.request(server)
-        .post('/login')
-        .send(userData)
-        .end((err,res)=>{
-            assert.equal(422,res.status);            
-        })
+            .post('/login')
+            .send(userData)
+            .end((err, res) => {
+                assert.equal(422, res.status);
+                done();
+            })
+    })
+
+    it('It will return success false', (done) => {
+        //this.timeout=10000;
+        let userData = {
+            email: "patildipakgmailcom",
+            password: "123"
+        }
+        chai.request(server)
+            .post('/login')
+            .send(userData)
+            .end((res) => {
+                assert.equal(false, res.responseResult.success);
+                done();
+            })
     })
 })
